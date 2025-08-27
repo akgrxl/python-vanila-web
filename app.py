939eed55162bd83235553e8bee7a6dba995ec86b
arunIreach <arun@ireachsol.com>
2025-08-27 19:25:14 +0530

    adding vanila code to create a web app in python

---
+++ app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import os
+
+PORT = 8000
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    
+    with socketserver.TCPServer(("", PORT), MyHTTPRequestHandler) as httpd:
+        print(f"Server running at http://localhost:{PORT}")
+        httpd.serve_forever()
